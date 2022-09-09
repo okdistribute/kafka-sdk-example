bdb6015137427d7f0b8c28a3dd4fe1b24b492c22
rae <633012+okdistribute@users.noreply.github.com>
2022-09-09 10:58:02 -0700

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,5 @@
 TOPIC=quickstart-events \
 ENDPOINT=localhost:9092 \
-APP_ID=ab695e18-ed99-4857-81d9-a956faa60715 \
+APP_ID= \
 CLIENT_ID=my-app \
-TOKEN=e87f520a-ed61-453b-9935-6a875714e7af node index.js
+TOKEN= node index.js
